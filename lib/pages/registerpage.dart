b6a30c888045b1651dacfc7fe2554667120a182b
Munir <munirkun123@gmail.com>
2023-02-16 21:34:04 +0700

    fix bug keyboard

--- lib/pages/registerpage.dart
+++ lib/pages/registerpage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+
 import 'package:get/get.dart';
 import 'package:komik_store/controllers/auth_controller.dart';
 import 'package:komik_store/routes/routes.dart';
@@ -14,107 +14,113 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     AuthController auth = Get.find();
     return Scaffold(
-        body: SafeArea(
-      child: Container(
-        width: double.infinity,
-        height: MediaQuery.of(context).size.height -
-            (MediaQuery.of(context).padding.top +
-                MediaQuery.of(context).padding.bottom),
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16,
-        ),
-        child: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minHeight: MediaQuery.of(context).size.height * 0.5,
-              maxHeight: MediaQuery.of(context).size.height -
-                  (MediaQuery.of(context).padding.top +
-                      MediaQuery.of(context).padding.bottom),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 24,
-                ),
-                Text(
-                  'Register',
-                  style: primaryTextStyle.copyWith(
-                      fontSize: 20, fontWeight: semiBold),
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                Container(
-                  width: 342,
-                  height: 245,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/register_logo.png'),
-                    ),
+        body: GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height -
+              (MediaQuery.of(context).padding.top +
+                  MediaQuery.of(context).padding.bottom),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: SingleChildScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height * 0.5,
+                maxHeight: MediaQuery.of(context).size.height -
+                    (MediaQuery.of(context).padding.top +
+                        MediaQuery.of(context).padding.bottom),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 24,
                   ),
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                CustomTextFormField(
-                    title: 'Name',
-                    hint: 'Input your name',
-                    controller: auth.nameC),
-                CustomTextFormField(
-                    title: "Email",
-                    hint: "Input your Email",
-                    controller: auth.emailCR),
-                CustomTextFormField(
-                  title: "Password",
-                  hint: "Input your password",
-                  controller: auth.passwordCR,
-                  obscureText: true,
-                ),
-                Expanded(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      width: double.infinity,
-                      height: MediaQuery.of(context).size.height * 0.12,
-                      padding: const EdgeInsets.symmetric(horizontal: 0),
-                      child: Column(
-                        children: [
-                          CustomButton(
-                            title: "Register",
-                            onPressed: () {
-                              auth.register();
-                            },
-                            textStyle: whiteTextStyle.copyWith(
-                                fontSize: 14, fontWeight: semiBold),
-                            bgColor: kBlueColor,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Have an Account?',
-                                style: secondaryTextStyle.copyWith(
-                                    fontSize: 14, fontWeight: regular),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  Get.toNamed(Routes.login);
-                                },
-                                child: Text(
-                                  'Login',
-                                  style: blueTextStyle.copyWith(
-                                      fontSize: 14, fontWeight: semiBold),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+                  Text(
+                    'Register',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 20, fontWeight: semiBold),
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  Container(
+                    width: 342,
+                    height: 245,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/register_logo.png'),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  CustomTextFormField(
+                      title: 'Name',
+                      hint: 'Input your name',
+                      controller: auth.nameC),
+                  CustomTextFormField(
+                      title: "Email",
+                      hint: "Input your Email",
+                      controller: auth.emailCR),
+                  CustomTextFormField(
+                    title: "Password",
+                    hint: "Input your password",
+                    controller: auth.passwordCR,
+                    obscureText: true,
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height * 0.12,
+                        padding: const EdgeInsets.symmetric(horizontal: 0),
+                        child: Column(
+                          children: [
+                            CustomButton(
+                              title: "Register",
+                              onPressed: () {
+                                auth.register();
+                              },
+                              textStyle: whiteTextStyle.copyWith(
+                                  fontSize: 14, fontWeight: semiBold),
+                              bgColor: kBlueColor,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  'Have an Account?',
+                                  style: secondaryTextStyle.copyWith(
+                                      fontSize: 14, fontWeight: regular),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Get.toNamed(Routes.login);
+                                  },
+                                  child: Text(
+                                    'Login',
+                                    style: blueTextStyle.copyWith(
+                                        fontSize: 14, fontWeight: semiBold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/pages/searchpage.dart
+++ lib/pages/searchpage.dart
@@ -18,98 +18,107 @@ class SearchPage extends StatelessWidget {
     GenreController genre = Get.find();
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            const WidgetSearch(),
-            Container(
-                padding: const EdgeInsets.only(top: 24, bottom: 24),
+      body: GestureDetector(
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              const WidgetSearch(),
+              Container(
+                  padding: const EdgeInsets.only(top: 24, bottom: 24),
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  child: Obx(() {
+                    if (genre.genreData.isEmpty) {
+                      return const CircularProgressIndicator();
+                    } else {
+                      return ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: genre.genreData.length,
+                        itemBuilder: (context, index) {
+                          return CustomCategoryGenre(
+                              name: genre.genreData[index].name.toString(),
+                              button: () {
+                                genre.findGenre(
+                                    genre.genreData[index].name.toString());
+                                comic.filterGenre();
+                              });
+                        },
+                      );
+                    }
+                  })),
+              Container(
+                margin: const EdgeInsets.only(bottom: 8),
                 width: double.infinity,
-                height: MediaQuery.of(context).size.height * 0.1,
+                child: Text(
+                  'Comic List',
+                  style:
+                      primaryTextStyle.copyWith(fontSize: 16, fontWeight: bold),
+                ),
+              ),
+              Expanded(
+                  child: SizedBox(
+                width: double.infinity,
                 child: Obx(() {
-                  if (genre.genreData.isEmpty) {
-                    return const CircularProgressIndicator();
-                  } else {
-                    return ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: genre.genreData.length,
+                  if (comic.foundComic.isEmpty && comic.comicData.isEmpty) {
+                    return const Center(
+                      child: LoadingIndicator(
+                          indicatorType: Indicator.ballClipRotatePulse),
+                    );
+                  } else if (comic.foundComic.isNotEmpty) {
+                    return GridView.builder(
+                      keyboardDismissBehavior:
+                          ScrollViewKeyboardDismissBehavior.onDrag,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              childAspectRatio: 0.5,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10),
+                      itemCount: comic.foundComic.length,
                       itemBuilder: (context, index) {
-                        return CustomCategoryGenre(
-                            name: genre.genreData[index].name.toString(),
-                            button: () {
-                              genre.findGenre(
-                                  genre.genreData[index].name.toString());
-                              comic.filterGenre();
-                            });
+                        return CustomCardComic(
+                          creator: comic.foundComic[index].creator,
+                          image: comic.foundComic[index].image,
+                          name: comic.foundComic[index].name,
+                          price: comic.foundComic[index].price,
+                          rating: comic.foundComic[index].rating,
+                          id: comic.foundComic[index].id,
+                          genres: comic.foundComic[index].genres[0].name,
+                        );
+                      },
+                    );
+                  } else {
+                    return GridView.builder(
+                      keyboardDismissBehavior:
+                          ScrollViewKeyboardDismissBehavior.onDrag,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              childAspectRatio: 0.5,
+                              crossAxisSpacing: 10,
+                              mainAxisSpacing: 10),
+                      itemCount: comic.comicData.length,
+                      itemBuilder: (context, index) {
+                        return CustomCardComic(
+                          creator: comic.comicData[index].creator,
+                          image: comic.comicData[index].image,
+                          name: comic.comicData[index].name,
+                          price: comic.comicData[index].price,
+                          rating: comic.comicData[index].rating,
+                          id: comic.comicData[index].id,
+                          genres: comic.comicData[index].genres[0].name,
+                        );
                       },
                     );
                   }
-                })),
-            Container(
-              margin: const EdgeInsets.only(bottom: 8),
-              width: double.infinity,
-              child: Text(
-                'Comic List',
-                style:
-                    primaryTextStyle.copyWith(fontSize: 16, fontWeight: bold),
-              ),
-            ),
-            Expanded(
-                child: SizedBox(
-              width: double.infinity,
-              child: Obx(() {
-                if (comic.foundComic.isEmpty && comic.comicData.isEmpty) {
-                  return const Center(
-                    child: LoadingIndicator(
-                        indicatorType: Indicator.ballClipRotatePulse),
-                  );
-                } else if (comic.foundComic.isNotEmpty) {
-                  return GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 0.5,
-                            crossAxisSpacing: 10,
-                            mainAxisSpacing: 10),
-                    itemCount: comic.foundComic.length,
-                    itemBuilder: (context, index) {
-                      return CustomCardComic(
-                        creator: comic.foundComic[index].creator,
-                        image: comic.foundComic[index].image,
-                        name: comic.foundComic[index].name,
-                        price: comic.foundComic[index].price,
-                        rating: comic.foundComic[index].rating,
-                        id: comic.foundComic[index].id,
-                        genres: comic.foundComic[index].genres[0].name,
-                      );
-                    },
-                  );
-                } else {
-                  return GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 0.5,
-                            crossAxisSpacing: 10,
-                            mainAxisSpacing: 10),
-                    itemCount: comic.comicData.length,
-                    itemBuilder: (context, index) {
-                      return CustomCardComic(
-                        creator: comic.comicData[index].creator,
-                        image: comic.comicData[index].image,
-                        name: comic.comicData[index].name,
-                        price: comic.comicData[index].price,
-                        rating: comic.comicData[index].rating,
-                        id: comic.comicData[index].id,
-                        genres: comic.comicData[index].genres[0].name,
-                      );
-                    },
-                  );
-                }
-              }),
-            ))
-          ],
+                }),
+              ))
+            ],
+          ),
         ),
       ),
     );
